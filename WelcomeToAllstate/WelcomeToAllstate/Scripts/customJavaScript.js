04a00c8535df50c153acf714261a5401eec2ae92
GARY-HP\Gary <garygumbleton1@gmail.com>
2017-02-12 21:46:01 +0000

    Move java script out to its own file

---
+++ WelcomeToAllstate/WelcomeToAllstate/Scripts/customJavaScript.js
@@ -0,0 +1,94 @@
+﻿$(document).ready(function () {
+    // Initialize Tooltip
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+})
+
+//Here Gary
+$(document).ready(function () {
+    $("#myModal").on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);  // Button that triggered the modal
+        var titleData = button.data('title'); // Extract value from data-* attributes
+        $(this).find('.modal-title').text(titleData);
+    });
+});
+function validateText(id) {
+    if ($("#" + id).val() == null || $("#" + id).val() == "") {
+        var div = $("#" + id).closest("div");
+        div.removeClass("has-success");
+        $("#glypcn" + id).remove();
+        div.addClass("has-error has-feedback");
+        div.append('<span id="glypcn' + id + '" class="glyphicon glyphicon-remove form-control-feedback"></span>');
+        return false;
+    }
+    else {
+        var div = $("#" + id).closest("div");
+        div.removeClass("has-error");
+        $("#glypcn" + id).remove();
+        div.addClass("has-success has-feedback");
+        div.append('<span id="glypcn' + id + '" class="glyphicon glyphicon-ok form-control-feedback"></span>');
+        return true;
+    }
+
+}
+
+function validateEmail(id) {
+    var email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+    if (!email_regex.test($("#" + id).val())) {
+        var div = $("#" + id).closest("div");
+        div.removeClass("has-success");
+        $("#glypcn" + id).remove();
+        div.addClass("has-error has-feedback");
+        div.append('<span id="glypcn' + id + '" class="glyphicon glyphicon-remove form-control-feedback"></span>');
+        return false;
+    }
+    else {
+        var div = $("#" + id).closest("div");
+        div.removeClass("has-error");
+        $("#glypcn" + id).remove();
+        div.addClass("has-success has-feedback");
+        div.append('<span id="glypcn' + id + '" class="glyphicon glyphicon-ok form-control-feedback"></span>');
+        return true;
+    }
+
+}
+
+$(document).ready(
+function () {
+    $("#applyButton").click(function () {
+        if (!validateText("contactName")) {
+            return false;
+        }
+        if (!validateEmail("contactEmail")) {
+            return false;
+        }
+        $("form#contactForm").submit();
+    }
+        );
+}
+
+
+    );
